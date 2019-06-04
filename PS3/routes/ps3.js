06e4c175eb78d67eaa7839532a003f153bd777f5
Jasmineyxsun <yxsun@bu.edu>
2019-06-04 19:51:30 -0400

    another small change

--- PS3/routes/ps3.js
+++ PS3/routes/ps3.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req,res,next) {
-  return res.render('string', {Test: 'Hey now'});
+  res.render('string', {Test: 'Hey now'});
 });
 router.post('/', function(req,res,next) {
   let returnString = req.body.string;
-  return res.render('string', {Test: returnString, length:returnString.length});
+  res.render('string', {Test: returnString, length:returnString.length});
 });
 
 module.exports = router;
