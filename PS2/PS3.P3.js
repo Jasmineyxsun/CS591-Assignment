037e4c6fe25b8e9148326f93198f1770a375dd37
Jasmineyxsun <yxsun@bu.edu>
2019-06-14 22:56:53 -0400

    delete previous file

--- PS2/PS3.P3.js
+++
@@ -1,6 +0,0 @@
-//A function that prints the cube value of its input
-const cube = input => input*input*input;
-//A single line of code to call the function on each value of the array [1,2,3,4,5,6,7]
-console.log([1,2,3,4,5,6,7].map(cube));
-
-
--- PS5/angular-tour-of-artists/src/app/app.module.ts
+++ PS5/angular-tour-of-artists/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ArtistsComponent } from './artists/artists.component';
+import { ArtistDetailComponent } from './artist-detail/artist-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArtistsComponent
+    ArtistsComponent,
+    ArtistDetailComponent
   ],
   imports: [
     BrowserModule,
